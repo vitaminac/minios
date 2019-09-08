d06eaee4dde68a273da1583d1cd946f54bad7d18
dalao <dalao1002@gmail.com>
2019-09-08 22:40:59 +0200

    formatting

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,57 +1,56 @@
-;
+    global _start
+    section .text
 ; A simple boot sector program
-
-; BIOS likes always to load 
-; the boot sector to the address 0x7c00
-
-; org directive tell the assembler 
-; where you expect the code will be loaded in memory
-; it will be used as a reference location
-; when you deference an address
-; assemble will precalculate offset added this value when it is compiled
-[org 0x7c00]
-
-; Stack Segment (SS). Pointer to the stack.
-; Code Segment (CS). Pointer to the code.
-; Data Segment (DS). Pointer to the data.
-; The address used in the instruction mov ax, [0x123f]
-; would by defualt offset from the data segment indexed by ds
-; Extra Segment (ES). Pointer to extra data ('E' stands for 'Extra').
-; F Segment (FS). Pointer to more extra data ('F' comes after 'E').
-; G Segment (GS). Pointer to still more extra data ('G' comes after 'F').
-
-; BIOS stores our boot drive in DL,
-; so it's best to remember this for later.
-mov [BOOT_DRIVE], dl
 
 ; Define a label, that will allow
 ; us to jump back to it
-program:
-; Typical lower memory layout afer boot
+_start:
 
-; Free Memory
-; ----------0x100000-----------
-; BIOS (256 KB)
-; ----------0xC0000------------
-; Video Memory (128 KB)
-; ----------0xA0000------------
-; Extended BIOS Data Area (639 KB)
-; ----------0x9fc00------------
-; Free (638 KB)
-; ----------0x7e00-------------
-; Loaded Boot Sector (512 Bytes)
-; ----------0x7c00-------------
-; Nothing
-; ----------0x500--------------
-; BIOS Data Area (256 Bytes)
-; ----------0x400--------------
-; Interrupt Vector Table (1 KB)
-; ----------0x0----------------
+    ; BIOS likes always to load 
+    ; the boot sector to the address 0x7c00
+
+    ; org directive tell the assembler 
+    ; where you expect the code will be loaded in memory
+    ; it will be used as a reference location
+    ; when you deference an address
+    ; assemble will precalculate offset added this value when it is compiled
+    [org 0x7c00]
+
+    ; Stack Segment (SS). Pointer to the stack.
+    ; Code Segment (CS). Pointer to the code.
+    ; Data Segment (DS). Pointer to the data.
+    ; The address used in the instruction mov ax, [0x123f]
+    ; would by default offset from the data segment indexed by ds
+    ; Extra Segment (ES). Pointer to extra data ('E' stands for 'Extra').
+    ; F Segment (FS). Pointer to more extra data ('F' comes after 'E').
+    ; G Segment (GS). Pointer to still more extra data ('G' comes after 'F').
+
+    ; BIOS stores our boot drive in DL,
+    ; so it's best to remember this for later.
+    mov [BOOT_DRIVE], dl
+
+    ; Typical lower memory layout afer boot
+    ; -----------------------------
+    ; Free Memory
+    ; ----------0x100000-----------
+    ; BIOS (256 KB)
+    ; ----------0xC0000------------
+    ; Video Memory (128 KB)
+    ; ----------0xA0000------------
+    ; Extended BIOS Data Area (639 KB)
+    ; ----------0x9fc00------------
+    ; Free (638 KB)
+    ; ----------0x7e00-------------
+    ; Loaded Boot Sector (512 Bytes)
+    ; ----------0x7c00-------------
+    ; Nothing
+    ; ----------0x500--------------
+    ; BIOS Data Area (256 Bytes)
+    ; ----------0x400--------------
+    ; Interrupt Vector Table (1 KB)
+    ; ----------0x0----------------
 
 ; declare some data in out program
-the_secret:
-    db "X"
-
 demonstrates_addressing:
     mov ah, 0x0e
     ; print the offset of the data
@@ -169,12 +168,12 @@ disk_read:
     mov sp, bp
     pop bp
 
-; Use a simple CPU instruction that jumps
-; to a new memory address to continue execution.
-; In our case , jump to the address of the current
-; instruction.
-; loops forever.
-hang: 
+hang:
+    ; Use a simple CPU instruction that jumps
+    ; to a new memory address to continue execution.
+    ; In our case , jump to the address of the current
+    ; instruction.
+    ; loops forever.
     jmp $
 
 ; Includes
@@ -182,34 +181,34 @@ hang:
 %include "disk_io.asm"
 
 ; Global Variables
-GREETING:
-    ; The zero on the end tells our routine
-    ; when to stop printing characters.
-    db 'Booting OS ', 0
-
+;    section .data
+; The zero on the end tells our routine
+; when to stop printing characters.
+the_secret: db "X"
+GREETING: db 'Booting OS ', 10, 0
 BOOT_DRIVE: db 0
 
-; >>>>>>>>>>> Bootsector padding and magic number <<<<<<<<<<
-; When compiled , our program must fit into 512 bytes,
-; with the last two bytes being the magic number,
-; so here , tell our assembly compiler to pad out our
-; program with enough zero bytes (db 0) to bring us to the
-; 510 th byte.
+    ; >>>>>>>>>>> Bootsector padding and magic number <<<<<<<<<<
+    ; When compiled , our program must fit into 512 bytes,
+    ; with the last two bytes being the magic number,
+    ; so here , tell our assembly compiler to pad out our
+    ; program with enough zero bytes (db 0) to bring us to the
+    ; 510 th byte.
 
-; $ is the address of the current position before emitting the bytes
-; $$ evaluates to the beginning of the current section
-; so ($−$$) tell you how far into the section
-; db #value# just the byte #value# 
-times 510 - ($ - $$) db 0
+    ; $ is the address of the current position before emitting the bytes
+    ; $$ evaluates to the beginning of the current section
+    ; so ($−$$) tell you how far into the section
+    ; db #value# just the byte #value# 
+    times 510 - ($ - $$) db 0
 
-; Last two bytes ( one word ) form the magic number,
-; so BIOS knows we are a boot sector.
-; dw #value# just the word #value# 
-dw 0xaa55
+    ; Last two bytes ( one word ) form the magic number,
+    ; so BIOS knows we are a boot sector.
+    ; dw #value# just the word #value# 
+    dw 0xaa55
 
-; We know that BIOS will load only the first 512 - byte sector from the disk ,
-; so if we purposely add a few more sectors to our code by repeating some
-; familiar numbers , we can prove to ourselfs that we actually loaded those
-; additional two sectors from the disk we booted from.
-times 256 dw 0xdada
-times 256 dw 0xface
+    ; We know that BIOS will load only the first 512 - byte sector from the disk ,
+    ; so if we purposely add a few more sectors to our code by repeating some
+    ; familiar numbers , we can prove to ourselfs that we actually loaded those
+    ; additional two sectors from the disk we booted from.
+    times 256 dw 0xdada
+    times 256 dw 0xface
