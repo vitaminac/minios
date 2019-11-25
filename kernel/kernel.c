da01bdaa43806efe7a076ec74c79c17e608c1c14
dalao <dalao1002@gmail.com>
2019-11-25 19:59:53 +0100

    keyboard

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,5 @@
 #include "drivers/screen.h"
+#include "drivers/keyboard.h"
 #include "libc/string.h"
 #include "cpu/isr.h"
 
@@ -9,12 +10,16 @@ void main()
     // and ensure pass argument to callee function
     // eax register is used to hold a function’s return value:
     print_at("Welcome to my first OS\n", 28, 12);
-    
+
     isr_install();
 
     // set timer interrupt frequency and init appropriate timer handler
     init_timer(50);
-    
+
+    /* Comment out the timer IRQ handler to read
+     * the keyboard IRQs easier */
+    init_keyboard();
+
     /* Test the interrupts */
     __asm__ __volatile__("int $2");
     __asm__ __volatile__("int $3");
